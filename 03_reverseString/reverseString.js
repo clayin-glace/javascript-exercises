844a008f0f384c54f2b6313678fa3ece78bdf69c
hw11chung <ddoldolry@gmail.com>
2022-10-14 14:58:30 -0700

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  stringArray = string.split("");
+  reversedStringArray = stringArray.reverse();
+  reversedString = reversedStringArray.toString();
 
+  return reversedString.replace(/,/g, ""); //removes all ','
 };
 
 // Do not edit below this line
