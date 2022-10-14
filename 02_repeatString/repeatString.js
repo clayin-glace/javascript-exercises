800b0ac4cfa78535cee6b6c625b4859a2af101bd
hw11chung <ddoldolry@gmail.com>
2022-10-13 18:16:06 -0700

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    outputString = "";
+
+    for (i = 0; i < num; i++) {
+      outputString = outputString.concat(string);
+    }
+  
+    return outputString;
+  }
+
 
 };
 
