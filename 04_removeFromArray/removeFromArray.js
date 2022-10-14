c257453b3a66cedc89ec1efea9532da4e254fdea
hw11chung <ddoldolry@gmail.com>
2022-10-14 14:54:56 -0700

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
 
+  for (removal of itemsToRemove) {
+    for (item of array) {
+      if (item === removal) {
+        item.array = array.splice(array.indexOf(item), 1);
+        console.log(array);
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
