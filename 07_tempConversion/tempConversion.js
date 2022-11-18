92997756ced8877c109f8790b935ac4b2d66aa62
hw11chung <ddoldolry@gmail.com>
2022-11-18 13:59:06 -0800

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) { //converts fahrenheit to celsius
+let tempC = (tempF - 32) * (5 / 9);
+return (Math.round(tempC * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) { //converts celsius to fahrenheit
+let tempF = (tempC * 1.8) + 32;
+return (Math.round(tempF * 10) / 10);
 };
 
 // Do not edit below this line
