ba2cf461b2effef583ca1ec61b81b50091c7ab9e
hw11chung <ddoldolry@gmail.com>
2023-02-10 13:20:51 -0800

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+  for (num of numbers) {
+    sum = sum + num;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  for (num of numbers) {
+    product = product * num;
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factorial = 1;
+  for (let i = x; i > 1; i--) {
+    factorial = factorial * i
+  };
+  return factorial;
 };
 
 // Do not edit below this line
